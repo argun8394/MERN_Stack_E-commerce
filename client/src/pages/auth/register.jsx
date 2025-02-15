30c4cfe947cc42bb588c0fde2f862c6d06d7eba6
argun8394 <argundoganali@gmail.com>
2025-02-16 01:08:09 +0300

    update register

--- client/src/pages/auth/register.jsx
+++ client/src/pages/auth/register.jsx
@@ -1,7 +1,7 @@
 import CommonForm from "@/components/common/form";
-// import { useToast } from "@/components/ui/use-toast";
 import { registerFormControls } from "@/config";
-// import { registerUser } from "@/store/auth-slice";
+import { useToast } from "@/hooks/use-toast";
+import { registerUser } from "@/store/auth-sclice";
 import { useState } from "react";
 import { useDispatch } from "react-redux";
 import { Link, useNavigate } from "react-router-dom";
@@ -16,7 +16,7 @@ function AuthRegister() {
   const [formData, setFormData] = useState(initialState);
   const dispatch = useDispatch();
   const navigate = useNavigate();
-  // const { toast } = useToast();
+  const { toast } = useToast();
 
   function onSubmit(event) {
     event.preventDefault();
