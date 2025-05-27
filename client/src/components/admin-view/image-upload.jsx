e9114da459b51b0fc2553637eae9f0b36f76319d
argun8394 <argundoganali@gmail.com>
2025-05-27 23:44:15 +0300

    image-upload

--- client/src/components/admin-view/image-upload.jsx
+++ client/src/components/admin-view/image-upload.jsx
@@ -1,14 +1,15 @@
 import { useRef } from "react";
 import { Input } from "../ui/input";
 import { Label } from "../ui/label";
+import { UploadCloudIcon } from "lucide-react";
 
-function ProductImageUpload({ 
+function ProductImageUpload({
     imageFile,
     setImageFile,
     imageLoadingState,
     uploadedImageUrl,
     setUploadedImageUrl,
-    setImageLoadingState, 
+    setImageLoadingState,
 }) {
 
     const inputRef = useRef(null);
@@ -16,6 +17,28 @@ function ProductImageUpload({
 
     function handleImageFileChange(event) {
         console.log(event.target.files, "event.target.files");
+
+        const selectedFile = event.target.files?.[0];
+        console.log(selectedFile);
+
+        if (selectedFile) setImageFile(selectedFile);
+    }
+
+    function handleDragOver(event) {
+        event.preventDefault();
+    }
+
+    function handleDrop(event) {
+        event.preventDefault();
+        const droppedFile = event.dataTransfer.files?.[0];
+        if (droppedFile) setImageFile(droppedFile);
+    }
+
+    function handleRemoveImage() {
+        setImageFile(null);
+        if (inputRef.current) {
+            inputRef.current.value = "";
+        }
     }
 
 
@@ -23,10 +46,28 @@ function ProductImageUpload({
     return (
         <div className="w-full max-w-md mx-auto">
             <Label className="text-lg font-semibold mb-2 block"> Upload Image</Label>
-            <div>
-                <Input id="image-upload" type="file" className="hidden"  ref={inputRef} 
+            <div className="border-2 border-dashed rounded-lg p-4"
+                onDragOver={handleDragOver}
+                onDrop={handleDrop}
+            >
+                <Input id="image-upload" type="file"
+                    className="hidden"
+                    ref={inputRef}
                     onChange={handleImageFileChange}
                 />
+
+                {!imageFile ? (
+                    <Label
+                        htmlFor="image-upload"
+                        flex flex-col items-center justify-center h-32 cursor-pointer
+                    >
+                        <UploadCloudIcon className="w-10 h-10 text-muted-foreground mb-2" />
+                        <span>Drag & drop or click to upload image</span>
+                    </Label>
+                ) :
+                    <div className="flex items-center justify-between">
+                    </div>
+                }
             </div>
         </div>
     );
